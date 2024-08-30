4cd47a5f94288b643255e06f93ca4ac952ef0e95
WDRshadow <i@wdr.im>
2024-08-30 09:48:33 +0200

    fix: bugs.

--- Lab01.py
+++ Lab01.py
@@ -5,7 +5,7 @@ def generate_data(input_num, output_num, num) -> (np.ndarray, np.ndarray):
     return np.random.rand(num, input_num), np.random.randint(0, 2, (num, output_num))
 
 
-def forward_perceptron(x: np.ndarray, w: np.ndarray, threshold:int) -> np.ndarray:
+def forward_perceptron(x: np.ndarray, w: np.ndarray, threshold: int) -> np.ndarray:
     return np.where(np.dot(x, w) > threshold, 1, 0)
 
 
@@ -18,9 +18,9 @@ def perception_learning(x: np.ndarray, y: np.ndarray, w: np.ndarray, study_rate:
     for i, y_unit in enumerate(y_pred):
         if y_unit != y[i]:
             if y[i] == 1:
-                w[i] += study_rate * x
+                w[:, i] += study_rate * x
             elif y[i] == 0:
-                w[i] -= study_rate * x
+                w[:, i] -= study_rate * x
             else:
                 raise ValueError("The value of y should be 0 or 1.")
     return w
@@ -30,7 +30,7 @@ def delta_rule_learning(x: np.ndarray, y: np.ndarray, w: np.ndarray, study_rate:
     y_pred = forward_delta_rule(x, w)
     for i, y_unit in enumerate(y_pred):
         e = y[i] - y_unit
-        w[i] += study_rate * e * x
+        w[:, i] += study_rate * e * x
     return w
 
 
@@ -54,8 +54,6 @@ if __name__ == '__main__':
         y_delta_rule = forward_delta_rule(x[i], w_delta_rule)
         test_result.append((y_perceptron, y_delta_rule))
     # calculate the accuracy
-    accuracy_perceptron = sum([1 for y_pred, y in test_result if y_pred == y]) / len(test_result)
+    accuracy_perceptron = sum([1 for y_perceptron, y_delta_rule in test_result if np.array_equal(y_perceptron, y[i])]) / len(
+        test_result)
     print(f"Accuracy of perceptron learning: {accuracy_perceptron}")
-
-
-
