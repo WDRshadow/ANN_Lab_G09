885dd41243fa75304322fd254efb9d17168b2b53
WDRshadow <i@wdr.im>
2024-09-01 22:49:53 +0200

    update: it could run now

--- Lab01/GraphingUtils.py
+++ Lab01/GraphingUtils.py
@@ -1,13 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def plot_config(ax, title="Generated Data"):
     ax.set_xlim(-2, 2)
-    ax.set_ylim(-2, 2) 
+    ax.set_ylim(-2, 2)
     ax.set_xlabel('x1')
     ax.set_ylabel('x2')
     ax.set_title(title)
     ax.legend()
+
 
 def plot_data(ax, class0, class1):
     """
@@ -16,18 +18,21 @@ def plot_data(ax, class0, class1):
     ax.scatter(class1[0, :], class1[1, :], color='blue', label='Class 1')
     ax.scatter(class0[0, :], class0[1, :], color='red', label='Class 0')
 
-def plot_line_from_vector(class0, class1, direction_vector: np.ndarray, point=np.array([0, 0]), title="Generated Data"):
+
+def plot_line_from_vector(class0: np.ndarray, class1: np.ndarray, direction_vector: np.ndarray, point=np.array([0, 0]), title="Generated Data"):
     """
     Plot a 2D line given a direction vector and a point.
 
     Parameters:
+        class0 (np.ndarray): Data points for class 0, shape (n, 2).
+        class1 (np.ndarray): Data points for class 1, shape (n, 2).
         direction_vector (np.ndarray): The direction vector [a, b] of the line.
         point (np.ndarray): A point [x0, y0] through which the line passes (default is the origin).
     """
     fig, ax = plt.subplots()
 
-    ax.quiver(point[0], point[1], direction_vector[0], direction_vector[1], 
-           angles='xy', scale_units='xy', scale=1, color='C0')
+    ax.quiver(point[0], point[1], direction_vector[0], direction_vector[1],
+              angles='xy', scale_units='xy', scale=1, color='C0')
     try:
         slope = -direction_vector[0] / direction_vector[1]
     except ZeroDivisionError:
@@ -35,7 +40,27 @@ def plot_line_from_vector(class0, class1, direction_vector: np.ndarray, point=np
 
     ax.axline(xy1=point, slope=slope, color='C0', label='Decision Line')
 
-    plot_data(ax, class0, class1)
+    plot_data(ax, class0.transpose(), class1.transpose())
+    plot_config(ax, title)
+
+    plt.show()
+
+
+def plot_line_from_weights(class0: np.ndarray, class1: np.ndarray, weights: np.ndarray, title="Perceptron Learning Data"):
+    """
+    Plot a 2D line given a weight vector.
+
+    Parameters:
+        weights (np.ndarray): The weight vector [w0, w1, w2] of the line. The line should be w0*x0 + w1*x1 + w2 = 0.
+    """
+    fig, ax = plt.subplots()
+
+    # Plot the decision line
+    x = np.linspace(-2, 2, 100)
+    y = (-weights[0] / weights[2]) - (weights[1] / weights[2]) * x
+    ax.plot(x, y, color='C0', label='Decision Line')
+
+    plot_data(ax, class0.transpose(), class1.transpose())
     plot_config(ax, title)
 
     plt.show()
--- Lab01/Lab01.py
+++ Lab01/Lab01.py
@@ -1,7 +1,7 @@
 import unittest
 
 import numpy as np
-from GraphingUtils import plot_line_from_vector
+from GraphingUtils import plot_line_from_vector, plot_line_from_weights
 from PointRecognition import *
 
 
@@ -14,6 +14,7 @@ class PerceptronLearning:
         output_num: the number of output units
         threshold: the threshold of the perceptron
         study_rate: the study rate of the perceptron
+        epochs: the number of epochs to train the perceptron
     """
 
     def __init__(self, input_num: int, output_num: int, threshold: float, study_rate: float = 0.1, epochs: int = 100):
@@ -29,10 +30,10 @@ class PerceptronLearning:
         One step of the perceptron learning algorithm
         """
         y_pred = self.forward(x)
-        # print("y_pred", y_pred, "and y?", y)
-        for i, y_unit in enumerate(y_pred):
-            # print("y_unit", y_unit, "i:", i)
-            self.w[:, i] += self.study_rate * (y[i] - y_unit) * np.insert(x, 0, 1)
+        for i, y_i in enumerate(y_pred):
+            for j, y_j in enumerate(y_i):
+                self.w[:, j] += self.study_rate * (y[i, j] - y_j) * np.insert(x[i], 0, 1)
+        # TODO: check if the classification is correct
 
     def learning_loop(self, data: (np.ndarray, np.ndarray)):
         """
@@ -41,9 +42,7 @@ class PerceptronLearning:
         """
         x, y = data
         for e in range(self.epochs):
-            for i in range(len(x)):
-                # print("x[i] and y[i]", x[i], y[i])
-                self._one_step(x[i], y[i])
+            self._one_step(x, y)
 
     def forward(self, x: np.ndarray) -> np.ndarray:
         """
@@ -52,9 +51,9 @@ class PerceptronLearning:
         :return: the output data
         """
         # Add bias term to input
-        x = np.insert(x, 0, 1)
+        x = np.insert(x, 2, 0, axis=1)
         # print("checking forward", x, self.w, np.dot(x, self.w))
-        return np.where(np.dot(x, self.w) > self.threshold, 1, 0)
+        return np.where(np.dot(self.w.transpose(), x.transpose()) > self.threshold, 1, 0).transpose()
 
 
 class DeltaRuleLearning:
@@ -66,8 +65,9 @@ class DeltaRuleLearning:
             input_num: the number of input units
             output_num: the number of output units
             study_rate: the study rate of the perceptron
+            epochs: the number of epochs to train the perceptron
         """
-        self.w = np.zeros((input_num, output_num))
+        self.w = np.zeros((input_num + 1, output_num))
         self.study_rate = study_rate
         self.input_num = input_num
         self.output_num = output_num
@@ -78,9 +78,11 @@ class DeltaRuleLearning:
         One step of the delta rule learning algorithm
         """
         y_pred = self.forward(x)
-        for i, y_unit in enumerate(y_pred):
-            e = y[i] - y_unit
-            self.w[:, i] += self.study_rate * e * np.insert(x, 0, 1)
+        for i, y_i in enumerate(y_pred):
+            for j, y_j in enumerate(y_i):
+                e = y[i, j] - y_j
+                self.w[:, j] += self.study_rate * e * np.insert(x[i], 0, 1)
+        # TODO: check if the classification is correct
 
     def learning_loop(self, data: (np.ndarray, np.ndarray)):
         """
@@ -89,8 +91,7 @@ class DeltaRuleLearning:
         """
         x, y = data
         for e in range(self.epochs):
-            for i in range(len(x)):
-                self._one_step(x[i], y[i])
+            self._one_step(x, y)
 
     def forward(self, x: np.ndarray) -> np.ndarray:
         """
@@ -99,8 +100,8 @@ class DeltaRuleLearning:
         :return: the output data
         """
         # Add bias term to input
-        x = np.insert(x, 0, 1)
-        return np.dot(x, self.w)
+        x = np.insert(x, 2, 0, axis=1)
+        return np.dot(self.w.transpose(), x.transpose()).transpose()
 
 
 class Test(unittest.TestCase):
@@ -109,7 +110,7 @@ class Test(unittest.TestCase):
         self.n = 10
         self.threshold = 0
         self.s_r = 0.1
-        self.input_layer_len = 10
+        self.input_layer_len = 2
         self.output_layer_len = 1
 
         self.data = self.generate_data()
@@ -119,12 +120,10 @@ class Test(unittest.TestCase):
         """
         Generate random data for two classes.
         # TODO: modularize for delta rule
-
-        Parameters:
-            No parameters for now
         
         Returns:
-            point_array (np.ndarray): Data points for the class, shape (2, n).
+            data (np.ndarray): Data points for the classes, shape (2*n, 2). Example: [[1, 2], [3, 4], ...]
+            labels (np.ndarray): Labels for the classes, shape (2*n, 1). Example: [[1], [0], ...]
         """
 
         mA = [1.0, -0.5]
@@ -136,16 +135,17 @@ class Test(unittest.TestCase):
         self.class0 = self.generate_array(self.n, mB, sigmaB)
 
         # Determine different ways to stack the classes
-        data = np.stack([self.class1, self.class0])
+        data = np.vstack([self.class1, self.class0])
 
         labels1 = np.full((self.n, 1), 1)
         labels0 = np.full((self.n, 1), 0)
 
-        labels = np.vstack([labels1, labels0])
+        labels = np.vstack((labels1, labels0))
 
         return data, labels
 
-    def generate_array(self, n, mean, sigma):
+    @staticmethod
+    def generate_array(n, mean, sigma) -> np.ndarray:
         """
         Generate random data for a class.
 
@@ -164,19 +164,19 @@ class Test(unittest.TestCase):
         point_array = np.array([
             np.random.randn(n) * sigma + mean[0],  # Feature 1 for the class
             np.random.randn(n) * sigma + mean[1]  # Feature 2 for the class
-        ])
+        ]).transpose()
 
         return point_array
 
-    # def test_perceptron_learning(self):
-    #     perceptron = PerceptronLearning(self.input_layer_len, self.output_layer_len, self.threshold, self.s_r)
-    #     perceptron.learning_loop(self.data)
-    # self.plot_line_from_vector(perceptron.w, title="Perceptron Learning Data")
+    def test_perceptron_learning(self):
+        perceptron = PerceptronLearning(self.input_layer_len, self.output_layer_len, self.threshold, self.s_r)
+        perceptron.learning_loop(self.data)
+        plot_line_from_weights(self.class0, self.class1, perceptron.w)
 
-    # def test_delta_rule_learning(self):
-    #     delta_rule = DeltaRuleLearning(self.input_layer_len, self.output_layer_len, self.s_r)
-    #     delta_rule.learning_loop(self.data)
-    #     self.plot_line_from_vector(delta_rule.w, title="Delta Rule Learning Data")
+    def test_delta_rule_learning(self):
+        delta_rule = DeltaRuleLearning(self.input_layer_len, self.output_layer_len, self.s_r)
+        delta_rule.learning_loop(self.data)
+        plot_line_from_weights(self.class0, self.class1, delta_rule.w)
 
     def test_test(self):
         direction_vector = [1, 0]
