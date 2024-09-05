6f9c2527ca458e7a8be5d603320bba06c08730f9
WDRshadow <i@wdr.im>
2024-09-06 00:34:42 +0200

    fix: bug of points recognition

--- Lab01/Lab01.py
+++ Lab01/Lab01.py
@@ -6,7 +6,7 @@ import numpy as np
 
 
 class LearningAlgorithm(ABC):
-    def __init__(self, input_num, output_num, study_rate=0.001, epochs=1000):
+    def __init__(self, input_num, output_num, study_rate=0.01, epochs=1000, threshold=0.0):
         """
         Basic learning algorithm
 
@@ -18,22 +18,31 @@ class LearningAlgorithm(ABC):
         """
         self.w = np.zeros((input_num + 1, output_num))
         self.study_rate = study_rate
+        self.threshold = threshold
         self.input_num = input_num
         self.output_num = output_num
         self.epochs = epochs
 
-    def _one_step(self, x: np.ndarray, y: np.ndarray):
+    def _one_step(self, x: np.ndarray, y: np.ndarray, learning_type="s"):
         """
         One step of the learning algorithm
         """
-        for i, y_i in enumerate(y):
-            y_pred = self(x[i])
-            for j, y_j in enumerate(y_i):
-                self.w[:, j] += self.study_rate * (y_j - y_pred[j]) * np.insert(x[i], 0, 1)
+        if learning_type == "s":
+            for i, y_i in enumerate(y):
+                y_pred = self(x[i])
+                for j, y_j in enumerate(y_i):
+                    x_bias = np.insert(x[i], 0, 1)
+                    self.w[:, j] += self.study_rate * (y_j - y_pred[j]) * x_bias
+        elif learning_type == "b":
+            y_pred = self(x)
+            for j in range(self.output_num):
+                x_bias = np.insert(x, 0, 1, axis=1)
+                self.w[:, j] += self.study_rate * np.sum((y[:, j] - y_pred[:, j]) * x_bias.transpose(), axis=1)
 
-    def train(self, data: (np.ndarray, np.ndarray)):
+    def train(self, data: (np.ndarray, np.ndarray), learning_type="s"):
         """
         The learning loop of the learning algorithm
+        :param learning_type: the type of the learning algorithm (s or b)
         :param data: the training data
         """
         x, y = data
@@ -44,33 +53,25 @@ class LearningAlgorithm(ABC):
             if self._is_all_points_on_propper_side(data):
                 print("all points are correct, n of epochs: ", e)
                 return
-
-            self._one_step(x, y)
+            self._one_step(x, y, learning_type)
         print("Values are not linearly separable in specified epochs.")
 
+    @abstractmethod
     def _is_all_points_on_propper_side(self, data: (np.ndarray, np.ndarray)) -> bool:
-        """
-        Check if all points are on the propper side of the decision line
-        :param data: the training data
-        :return: True if all points are on the propper side of the decision line, False otherwise
-        """
-        x, y = data
-        y_pred = self(x)
-        return np.array_equal(y, y_pred)
+        pass
 
     @abstractmethod
     def __call__(self, x: np.ndarray) -> np.ndarray:
         pass
 
-    def plot(self, class0: np.ndarray, class1: np.ndarray, title="Generated Data"):
+    def plot(self, data: (np.ndarray, np.ndarray), title="Generated Data"):
         """
         Plot a 2D line given a direction vector and a point.
 
         The line should be 0 = bias + a*x1 + b*x2.
 
         Parameters:
-            class0 (np.ndarray): Data points for class 0, shape (n, 2).
-            class1 (np.ndarray): Data points for class 1, shape (n, 2).
+            data (np.ndarray): The data to plot.
             title (str): Title of the plot.
         """
         fig, ax = plt.subplots()
@@ -89,43 +90,33 @@ class LearningAlgorithm(ABC):
 
         ax.axline(xy1=point, slope=slope, color='C0', label='Decision Line')
 
-        def plot_config(ax, title="Generated Data"):
-            ax.set_xlim(-2, 2)
-            ax.set_ylim(-2, 2)
-            ax.set_xlabel('x1')
-            ax.set_ylabel('x2')
-            ax.set_title(title)
-            ax.legend()
+        labels = data[1].flatten()
+        class1 = data[0][labels == 1]
+        class0 = data[0][labels == 0]
 
-        def plot_data(ax, class0, class1):
-            """
-            Plot the generated data for class 1 and class 0.
-            """
-            ax.scatter(class1[0, :], class1[1, :], color='blue', label='Class 1')
-            ax.scatter(class0[0, :], class0[1, :], color='red', label='Class 0')
+        ax.scatter(class1.transpose()[0, :], class1.transpose()[1, :], color='blue', label='Class 1')
+        ax.scatter(class0.transpose()[0, :], class0.transpose()[1, :], color='red', label='Class 0')
 
-        plot_data(ax, class0.transpose(), class1.transpose())
-        plot_config(ax, title)
+        ax.set_xlim(-2, 2)
+        ax.set_ylim(-2, 2)
+        ax.set_xlabel('x1')
+        ax.set_ylabel('x2')
+        ax.set_title(title)
+        ax.legend()
 
         plt.show()
 
 
 class PerceptronLearning(LearningAlgorithm):
-    """
-    Perceptron learning algorithm
-
-    Parameters:
-        input_num: the number of input units
-        output_num: the number of output units
-        threshold: the threshold of the perceptron
-        study_rate: the study rate of the perceptron
-        epochs: the number of epochs to train the perceptron
-    """
-
-    def __init__(self, input_num: int, output_num: int, threshold: float = 0, study_rate: float = 0.1,
-                 epochs: int = 3000):
-        super().__init__(input_num, output_num, study_rate, epochs)
-        self.threshold = threshold
+    def _is_all_points_on_propper_side(self, data: (np.ndarray, np.ndarray)) -> bool:
+        """
+        Check if all points are on the propper side of the decision line
+        :param data: the training data
+        :return: True if all points are on the propper side of the decision line, False otherwise
+        """
+        x, y = data
+        y_pred = self(x)
+        return np.array_equal(y, y_pred)
 
     def __call__(self, x: np.ndarray) -> np.ndarray:
         """
@@ -142,6 +133,20 @@ class PerceptronLearning(LearningAlgorithm):
 
 
 class DeltaRuleLearning(LearningAlgorithm):
+    def _is_all_points_on_propper_side(self, data: (np.ndarray, np.ndarray)) -> bool:
+        """
+        Check if all points are on the propper side of the decision line
+        :param data: the training data
+        :return: True if all points are on the propper side of the decision line, False otherwise
+        """
+        x, y = data
+        if self.w[2] == 0:
+            return False
+        # x2 = (-bias - a*x1) / b
+        x2 = (-self.w[0] - self.w[1] * x.transpose()[0]) / self.w[2]
+        y_pred = np.where(x.transpose()[1] < x2, 0, 1).reshape(-1, 1)
+        return np.array_equal(y, y_pred)
+
     def __call__(self, x: np.ndarray) -> np.ndarray:
         """
         The forward pass of the perceptron
@@ -156,49 +161,33 @@ class DeltaRuleLearning(LearningAlgorithm):
         return np.dot(self.w.transpose(), x.transpose()).transpose()
 
 
-class Test(unittest.TestCase):
-    def __init__(self, *args, **kwargs):
-        super(Test, self).__init__(*args, **kwargs)
-        self.n = 10
-        self.input_layer_len = 2
-        self.output_layer_len = 1
+class DataGenerator:
+    """
+    Data generator for the perceptron learning algorithm
 
-        self.mA = [-1.0, -0.5]
-        self.sigmaA = 0.5
-        self.mB = [1.0, 0.5]
-        self.sigmaB = 0.5
+    Properties:
+        class1 (np.ndarray): the data points for class 1 \n
+        class0 (np.ndarray): the data points for class 0 \n
+        data (np.ndarray): the data points and their labels
+    """
+    def __init__(self, n=100, mA=None, sigmaA = 0.5, mB=None, sigmaB = 0.5):
+        self.n = n
 
-        self.data = self.generate_data()
-        self.randomly_mix_data()
+        if mA is None:
+            mA = [1.0, 0.5]
+        if mB is None:
+            mB = [-1.0, -0.5]
+        self.mA = mA
+        self.sigmaA = sigmaA
+        self.mB = mB
+        self.sigmaB = sigmaB
 
-    def generate_data(self):
+        self.data = self._generate_data()
+        self._randomly_mix_data()
 
-        self.class1 = self.generate_array(self.n, self.mA, self.sigmaA)
-        self.class0 = self.generate_array(self.n, self.mB, self.sigmaB)
-
-        # Determine different ways to stack the classes
-        data = np.vstack([self.class1, self.class0])
-
-        labels1 = np.full((self.n, 1), 1)
-        labels0 = np.full((self.n, 1), 0)
-
-        labels = np.vstack((labels1, labels0))
-
-        return data, labels
-
-    def randomly_mix_data(self):
-        data = np.hstack([self.data[0], self.data[1]])
-
-        # Randomly mix the data points and their labels
-        np.random.shuffle(data)
-
-        # Split the data points and their labels
-        coords, labels = np.hsplit(data, [2])
-
-        self.data = coords, labels
 
     @staticmethod
-    def generate_array(n, mean, sigma) -> np.ndarray:
+    def _generate_array(n, mean, sigma) -> np.ndarray:
         # Convert means to NumPy arrays
         mean = np.array(mean)
 
@@ -210,15 +199,57 @@ class Test(unittest.TestCase):
 
         return point_array
 
+    def _generate_data(self):
+
+        class1 = self._generate_array(self.n, self.mA, self.sigmaA)
+        class0 = self._generate_array(self.n, self.mB, self.sigmaB)
+
+        # Determine different ways to stack the classes
+        data = np.vstack([class1, class0])
+
+        labels1 = np.full((self.n, 1), 1)
+        labels0 = np.full((self.n, 1), 0)
+
+        labels = np.vstack((labels1, labels0))
+
+        return data, labels
+
+    def _randomly_mix_data(self):
+        data = np.hstack([self.data[0], self.data[1]])
+
+        # Randomly mix the data points and their labels
+        np.random.shuffle(data)
+
+        # Split the data points and their labels
+        coords, labels = np.hsplit(data, [2])
+
+        self.data = coords, labels
+
+
+
+class Test(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        super(Test, self).__init__(*args, **kwargs)
+        self.study_rate = 0.01
+        self.epochs = 200
+
+        self.data_generator = DataGenerator(
+            n=10,
+            mA=[1.0, 0.5],
+            sigmaA = 0.5,
+            mB=[-1.0, -0.5],
+            sigmaB = 0.5
+        )
+
     def test_perceptron_learning(self):
-        perceptron = PerceptronLearning(self.input_layer_len, self.output_layer_len)
-        perceptron.train(self.data)
-        perceptron.plot(self.class0, self.class1, title="Perceptron Learning Data")
+        perceptron = PerceptronLearning(2, 1, study_rate=self.study_rate, epochs=self.epochs)
+        perceptron.train(self.data_generator.data)
+        perceptron.plot(self.data_generator.data, title="Perceptron Learning Data")
 
     def test_delta_rule_learning(self):
-        delta_rule = DeltaRuleLearning(self.input_layer_len, self.output_layer_len)
-        delta_rule.train(self.data)
-        delta_rule.plot(self.class0, self.class1, title="Delta Rule Learning Data")
+        delta_rule = DeltaRuleLearning(2, 1, study_rate=self.study_rate, epochs=self.epochs)
+        delta_rule.train(self.data_generator.data, learning_type="b")
+        delta_rule.plot(self.data_generator.data, title="Delta Rule Learning Data")
 
     def test_all(self):
         self.test_perceptron_learning()
