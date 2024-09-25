69f07dc9978c7a3eca0da2ab4f76105e6fc702ed
Gašpar Haramija <gaspar.haramija@gmail.com>
2024-09-25 21:20:39 +0200

    added functions for displaying graphs for part 1

--- Lab02/part1.py
+++ Lab02/part1.py
@@ -18,10 +18,13 @@ class Test(unittest.TestCase):
     def __init__(self, *args, **kwargs):
         super(Test, self).__init__(*args, **kwargs)
         self.samples = 100
+
         self.data_generator_sin2x = One_Dim_Function(0, 2 * np.pi, self.samples, function=lambda x: np.sin(2 * x))
         self.data_generator_square2x = One_Dim_Function(-50, 50, self.samples, function=lambda x: (2 * x) ** 2)
-        self.ballist: np.ndarray = Read_Files("data_lab2/ballist.dat").read_matrix(" ", "\t", float)
-        self.balltest: np.ndarray = Read_Files("data_lab2/balltest.dat").read_matrix(" ", "\t", float)
+        self.data_generator_square2xbox = One_Dim_Function(0, 2 * np.pi, self.samples, function=lambda x: np.where(np.sin(x) >= 0, 1, -1))
+
+        # self.ballist: np.ndarray = Read_Files("data_lab2/ballist.dat").read_matrix(" ", "\t", float)
+        # self.balltest: np.ndarray = Read_Files("data_lab2/balltest.dat").read_matrix(" ", "\t", float)
 
     @staticmethod
     def _test(data_generator: One_Dim_Function, model: RBF, rbf_unit=10, sigma=1.0, competitive_learning=False,
@@ -88,3 +91,98 @@ class Test(unittest.TestCase):
         rbf_net = RBF(input_dim=2, rbf_dim=rbf_unit)
         rbf_net.train(X_train, y_train)
         rbf_net.test(X_test, y_test)
+
+    def test_rbf_residuals_vs_units(self):
+        rbf_units_range = range(1, 21)
+        
+        residuals_sin2x = []
+        residuals_square2xbox = []
+        residuals_sin2x_noise = []
+        residuals_square2xbox_noise = []
+
+        sin2x_gen = self.data_generator_sin2x
+        square2xbox_gen = self.data_generator_square2xbox
+        
+        sin2x_noise_gen = One_Dim_Function(0, 2 * np.pi, self.samples, function=lambda x: np.sin(2 * x))
+        square2xbox_noise_gen = One_Dim_Function(0, 2 * np.pi, self.samples, function=lambda x: np.where(np.sin(x) >= 0, 1, -1))
+        sin2x_noise_gen.add_gaussian_noise(0, 0.5)
+        square2xbox_noise_gen.add_gaussian_noise(0, 0.5)
+
+        for rbf_unit in rbf_units_range:
+            rbf_sin2x = RBF(input_dim=1, rbf_dim=rbf_unit)
+            rbf_square2xbox = RBF(input_dim=1, rbf_dim=rbf_unit)
+            rbf_sin2x_noise = RBF(input_dim=1, rbf_dim=rbf_unit)
+            rbf_square2xbox_noise = RBF(input_dim=1, rbf_dim=rbf_unit)
+            
+            X_train_sin2x, y_train_sin2x = sin2x_gen.data
+            X_train_square2xbox, y_train_square2xbox = square2xbox_gen.data
+            centers_sin2x = np.linspace(X_train_sin2x[0], X_train_sin2x[-1], rbf_unit).reshape(-1, 1)
+            centers_square2xbox = np.linspace(X_train_square2xbox[0], X_train_square2xbox[-1], rbf_unit).reshape(-1, 1)
+
+            rbf_sin2x.set_C_n_Sigma(centers_sin2x, np.array([1.0] * rbf_unit))
+            rbf_square2xbox.set_C_n_Sigma(centers_square2xbox, np.array([1.0] * rbf_unit))
+            rbf_sin2x_noise.set_C_n_Sigma(centers_sin2x, np.array([1.0] * rbf_unit))
+            rbf_square2xbox_noise.set_C_n_Sigma(centers_square2xbox, np.array([1.0] * rbf_unit))
+
+            rbf_sin2x.backward(X_train_sin2x, y_train_sin2x)
+            rbf_square2xbox.backward(X_train_square2xbox, y_train_square2xbox)
+            rbf_sin2x_noise.backward(sin2x_noise_gen.data[0], sin2x_noise_gen.data[1])
+            rbf_square2xbox_noise.backward(square2xbox_noise_gen.data[0], square2xbox_noise_gen.data[1])
+
+            residuals_sin2x.append(np.mean((rbf_sin2x(sin2x_gen.data[0]) - sin2x_gen.data[1]) ** 2))
+            residuals_square2xbox.append(np.mean((rbf_square2xbox(square2xbox_gen.data[0]) - square2xbox_gen.data[1]) ** 2))
+            residuals_sin2x_noise.append(np.mean((rbf_sin2x_noise(sin2x_noise_gen.data[0]) - sin2x_noise_gen.data[1]) ** 2))
+            residuals_square2xbox_noise.append(np.mean((rbf_square2xbox_noise(square2xbox_noise_gen.data[0]) - square2xbox_noise_gen.data[1]) ** 2))
+
+        plt.figure()
+        plt.plot(rbf_units_range, residuals_sin2x, label="sin(2x)", marker='o', color='blue')
+        plt.plot(rbf_units_range, residuals_square2xbox, label="square(2x)box", marker='o', color='green')
+        plt.plot(rbf_units_range, residuals_sin2x_noise, label="sin(2x) with noise", marker='o', color='red')
+        plt.plot(rbf_units_range, residuals_square2xbox_noise, label="square(2x)box with noise", marker='o', color='orange')
+        
+        plt.title('Residual Error vs Number of RBF Units')
+        plt.xlabel('Number of RBF Units')
+        plt.ylabel('Residual Error (MSE)')
+        plt.grid(True)
+        plt.legend()
+        plt.show()
+
+    def test_rbf_residuals_vs_units_sigma(self):
+        rbf_units_range = range(1, 21)
+        sigma_values = [0.1, 0.3, 0.6, 1.0, 2.0]
+        
+        sin2x_gen = self.data_generator_sin2x
+        square2xbox_gen = self.data_generator_square2xbox
+        
+        sin2x_noise_gen = One_Dim_Function(0, 2 * np.pi, self.samples, function=lambda x: np.sin(2 * x))
+        square2xbox_noise_gen = One_Dim_Function(0, 2 * np.pi, self.samples, function=lambda x: np.where(np.sin(x) >= 0, 1, -1))
+        sin2x_noise_gen.add_gaussian_noise(0, 0.1)
+        square2xbox_noise_gen.add_gaussian_noise(0, 0.1)
+
+        generators = [(sin2x_gen, "sin(2x)"), (square2xbox_gen, "square(2x)box"),
+                      (sin2x_noise_gen, "sin(2x) with noise"), (square2xbox_noise_gen, "square(2x)box with noise")]
+
+        for gen, label in generators:
+            plt.figure()
+            for sigma in sigma_values:
+                residual_errors = []
+
+                for rbf_unit in rbf_units_range:
+                    rbf_net = RBF(input_dim=1, rbf_dim=rbf_unit)
+                    
+                    X_train, y_train = gen.data
+                    centers = np.linspace(X_train[0], X_train[-1], rbf_unit).reshape(-1, 1)
+                    rbf_net.set_C_n_Sigma(centers, np.array([sigma] * rbf_unit))
+                    rbf_net.backward(X_train, y_train)
+
+                    residual_errors.append(np.mean((rbf_net(gen.data[0]) - gen.data[1]) ** 2))
+
+                plt.plot(rbf_units_range, residual_errors, label=f"Sigma = {sigma}", marker='o')
+
+            plt.title(f'Residual Error vs Number of RBF Units for {label}')
+            plt.xlabel('Number of RBF Units')
+            plt.ylabel('Residual Error (MSE)')
+            plt.grid(True)
+            plt.legend()
+            plt.show()
+
