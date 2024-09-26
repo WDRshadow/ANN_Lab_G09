306079c5188d7882f0b62a61515164f98e7ce672
WDRshadow <i@wdr.im>
2024-09-26 12:43:05 +0200

    update: seq mode for delta rule

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,6 +1,6 @@
 from .ActivationFunction import ActivationFunction, Sigmoid, ReLU, Tanh
 from .DataGenerator import DataGenerator, DataGenerator2, MackeyGlass, GaussFunctionData, One_Dim_Function, Read_Files
 from .MLP import MLP, Layer
-from .RBF import RBF
+from .RBF import RBF, RBF_Delta_Rule
 from .SLP import SingleLevelPerceptron, PerceptronLearning, DeltaRuleLearning
 from .SOM import SOM_2D
