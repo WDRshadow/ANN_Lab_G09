5fff9165a1cb760317eb1cd44414bbbb2ccee3a0
Gašpar Haramija <gaspar.haramija@gmail.com>
2024-10-02 15:06:48 +0200

    parts 3_3 and 3_4

--- Lab03/Lab03.py
+++ Lab03/Lab03.py
@@ -120,3 +120,134 @@ class TestHopfield(unittest.TestCase):
         images = self.data_generator.data.reshape((11, 1024))
 
         print_image(images[2].reshape((32, 32)))
+
+
+    def test_33_1(self):
+        images = self.data_generator.data.reshape((11, 1024))
+        p1 = images[0]
+        p2 = images[1]
+        p3 = images[2]
+        p10 = images[9]
+        p11 = images[10]
+
+        hopfield = HopfieldNetwork(1024)
+        hopfield.train(images[:3])
+
+        print_image(p1.reshape(32,32))
+        
+        energy_p1 = hopfield.energy(p1)
+        energy_p2 = hopfield.energy(p2)
+        energy_p3 = hopfield.energy(p3)
+        energy_p10 = hopfield.energy(p10)
+        energy_p11 = hopfield.energy(p11)
+
+        print(energy_p1)
+        print(energy_p2)
+        print(energy_p3)
+        print(energy_p10)
+        print(energy_p11)
+
+        # recalled_p1 = hopfield.recall(p1, steps = 100)
+        recalled_p10, energies_p10 = hopfield.recall(p10, steps = 3, energy_list=True)
+        recalled_p11, energies_p11 = hopfield.recall(p11, steps = 3, energy_list=True)
+
+        # print_image(recalled_p1.reshape(32,32))
+        print_image(recalled_p10.reshape(32,32))
+        print_image(recalled_p11.reshape(32,32))
+
+        iterations = range(len(energies_p11)) 
+        plt.plot(iterations, energies_p10, label="p10", color='green', linestyle='--', marker='x')
+        plt.plot(iterations, energies_p11, label="p11", color='red', linestyle='-.', marker='s')
+        plt.xlabel('Iterations')
+        plt.ylabel('Energy')
+        plt.title('Energy vs. Iterations')
+        plt.legend()
+        plt.show()
+
+
+    def test_33_2(self):
+        images = self.data_generator.data.reshape((11, 1024))
+        p1 = images[0]
+        p2 = images[1]
+        p3 = images[2]
+        p10 = images[9]
+        p11 = images[10]
+
+        arbitrary_start = np.random.choice([1, -1], (1024,1))
+
+        hopfield_1 = HopfieldNetwork(1024)
+        hopfield_1.weights = hopfield_1.initialize_weights(random_gauss=True, symmetric=False)
+        hopfield_2 = HopfieldNetwork(1024)
+        hopfield_2.weights = np.multiply(0.5, np.add(hopfield_1.weights, hopfield_1.weights.T))
+        # hopfield_2.weights = hopfield_2.initialize_weights(random_gauss=True, symmetric=True)
+        hopfield_1.train(images[:3])
+        hopfield_2.train(images[:3])
+
+
+
+        recalled_1, energies_1 = hopfield_1.recall(arbitrary_start, steps = 3, energy_list=True)
+        recalled_2, energies_2 = hopfield_2.recall(arbitrary_start, steps = 3, energy_list=True)
+
+        if np.array_equal(recalled_1, recalled_2):
+            print("equal")
+        else:
+            print("notequal")
+
+        iterations = range(len(energies_1)) 
+        plt.plot(iterations, energies_1, label="Non symmetric", color='green')
+        plt.plot(iterations, energies_2, label="Symmetric", color='red')
+        plt.xlabel('Iterations')
+        plt.ylabel('Energy')
+        plt.title('Energy vs. Iterations')
+        plt.legend()
+        plt.show()
+
+
+    def test_34(self):
+        images = self.data_generator.data.reshape((11, 1024))
+        p1 = images[0]
+        p2 = images[1]
+        p3 = images[2]
+        hopfield = HopfieldNetwork(1024)
+        hopfield.train(images[:3])
+
+        def add_noise(pattern, num_flip):
+            noisy_pattern = pattern.copy()
+            flip_indices = np.random.choice(1024, num_flip, replace=False)
+            noisy_pattern[flip_indices] *= -1
+            return noisy_pattern
+
+        for noise_level in np.arange(0, 1.1, 0.1):
+
+            print("CURRENT NOISE LEVEL IS ")
+            print(noise_level)
+
+            num_flip = int(noise_level * 1024)
+
+            noisy_p1 = add_noise(p1, num_flip)
+            noisy_p2 = add_noise(p2, num_flip)
+            noisy_p3 = add_noise(p3, num_flip)
+
+            recp1 = hopfield.recall(noisy_p1, asyncronous=False)
+            recp2 = hopfield.recall(noisy_p2, asyncronous=False)
+            recp3 = hopfield.recall(noisy_p3, asyncronous=False)
+
+            print("Original Pattern (p1):\n" + np.array2string(p1) + 
+            "\nNoisy Pattern (noisy_p1):\n" + np.array2string(noisy_p1) + 
+            "\nRecalled Pattern (recp1):\n" + np.array2string(recp1))
+
+
+            if np.array_equal(p1, recp1):
+                print("P1 equal to recall P1")
+            else:
+                print("P1 not equal to recall p1")
+
+            if np.array_equal(p2, recp2):
+                print("P2 equal to recall P2")
+            else:
+                print("P2 not equal to recall p2")
+
+            if np.array_equal(p3, recp3):
+                print("P3 equal to recall P3")
+            else:
+                print("P3 not equal to recall p3")
--- utils/HopfieldNetwork.py
+++ utils/HopfieldNetwork.py
@@ -5,6 +5,14 @@ class HopfieldNetwork:
         self.n_units = n_units
         self.weights = np.zeros((n_units, n_units))
 
+    def initialize_weights(self, random_gauss=False, symmetric=True):
+        if random_gauss:
+            w = np.random.normal(0, 1, (self.n_units, self.n_units))
+            np.fill_diagonal(w, 0)
+            if symmetric:
+                w = np.multiply(0.5, np.add(w, w.T))
+            return w
+
     def train(self, patterns):
         for pattern in patterns:
             pattern = np.array(pattern)
@@ -12,24 +20,39 @@ class HopfieldNetwork:
         np.fill_diagonal(self.weights, 0)
         self.weights /= len(patterns)
 
-    def update(self, state, steps=1, random=False, asyncronous=True):
-        aux_state = state
-        print(asyncronous)
+        print("!!!!!!!!!!!!")
+        print(self.weights)
+
+    def update(self, state, steps=1, random=False, asyncronous=True, energy_list=False):
+        aux_state = np.copy(state)
+        energies = [self.energy(aux_state)]
+        
         for _ in range(steps):
             indices = range(self.n_units)
-            if random: indices = np.random.permutation(self.n_units)
+            if random: 
+                indices = np.random.permutation(self.n_units)
 
             if asyncronous:
                 for i in indices:
                     raw_input = np.dot(self.weights[i], state)
                     state[i] = 1 if raw_input >= 0 else -1
+                    if energy_list:
+                        energies.append(self.energy(state))
             else:
                 for i in indices:
                     raw_input = np.dot(self.weights[i], state)
                     aux_state[i] = 1 if raw_input >= 0 else -1
-                state = aux_state
+                state = np.copy(aux_state)
+                # energies.append(self.energy(state))
+        if energy_list:
+            return state, energies
         return state
 
-    def recall(self, pattern, steps=10, random=False, asyncronous=True):
+    def recall(self, pattern, steps=10, random=False, asyncronous=True, energy_list=False):
         state = np.copy(pattern)
-        return self.update(state, steps, random, asyncronous)
+        return self.update(state, steps, random, asyncronous, energy_list)
+    
+    def energy(self, state):
+        product = np.outer(state, np.transpose(state))
+        energy_sum = np.sum(np.multiply(self.weights, product))
+        return -energy_sum
